bb6aaca7f58daef164d7319788cdc0c9295cd219
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-28 18:45:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-!!text.match(/\A[A-Z].{1,100}[,.!}]\Z/)
+!!text.match(/\A[A-Z].{1,100}[,.!?]\Z/)
 end
 
 def valid_phone_number?(phone)
