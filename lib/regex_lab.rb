89be8d6eb7779a58832fc4dfaf654c903efdfa19
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-28 19:08:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/(\w\b)[0-9]{10}\b/) 
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+end
 end
